2c7a1f85f121df243a54922c8398a2010cfa7086
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-13 23:02:05 +0000

    Add script to retrieve manifests

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 import { Container, Input } from 'semantic-ui-react'
-//import 'semantic-ui-css/components/container.css'
-//import 'semantic-ui-css/components/input.css'
-//import 'semantic-ui-css/components/icon.css'
 import 'semantic-ui-css/semantic.css'
 
 const Home = () => (
@@ -17,15 +14,20 @@ const Home = () => (
         <img src="/logo.svg" />
       </div>
     </Container>
-      <div className="section">
-    <Input size="huge" placeholder="Search..." />
+    <div className="section">
+      <div className="container">
+        <Input fluid size="huge" placeholder="Search..." />
       </div>
+    </div>
 
     <style jsx>{`
       .section {
         width: 100%;
         display: flex;
         justify-content: center;
+      }
+      .section > .container {
+        width: 500px;
       }
     `}</style>
   </>
