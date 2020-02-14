d40688720807e9605b9b68a44b83b93976af2ef9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-14 00:37:57 +0000

    Get more info into card

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -2,21 +2,28 @@ import React from 'react'
 import Head from 'next/head'
 import { Card, Image } from 'semantic-ui-react'
 
-const ProjectCard = ({ project }) => (
-  <Card key={project.id} as="a" href={project['documentation-home']}>
-    <Image
-      as="div"
-      style={{ height: 200, overflow: 'hidden' }}
-      src={project.image || '/images/placeholder.png'}
-    />
-    <Card.Content>
-      <Card.Header>{project.title}</Card.Header>
-      <Card.Meta>
-        <span className="date">x</span>
-      </Card.Meta>
-      <Card.Description>lorem ipsum</Card.Description>
-    </Card.Content>
-  </Card>
-)
+const ProjectCard = ({ project }) => {
+  const author =
+    project.licensor?.name ||
+    project.contributors?.[0]?.name ||
+    project['manifest-author']?.name ||
+    ''
+  return (
+    <Card key={project.id} as="a" href={project['documentation-home']}>
+      <Image
+        as="div"
+        style={{ height: 200, overflow: 'hidden' }}
+        src={project.image || '/images/placeholder.png'}
+      />
+      <Card.Content>
+        <Card.Header>{project.title}</Card.Header>
+        <Card.Meta>
+          <span className="date">{author}</span>
+        </Card.Meta>
+        <Card.Description>{project.description || ''}</Card.Description>
+      </Card.Content>
+    </Card>
+  )
+}
 
 export default ProjectCard
