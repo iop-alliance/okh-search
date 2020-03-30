eddd48cfcc01645ee9c4c71c1593e8fee5fd577c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-03-30 18:15:26 +0100

    Try blur effect when loading

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 import { Card } from 'semantic-ui-react'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 const ProjectCard = ({ project }) => {
   const author =
@@ -29,7 +30,11 @@ const ProjectCard = ({ project }) => {
           alignItems: 'center',
         }}
       >
-        <LazyLoadImage src={project.image || '/images/placeholder.png'} />
+        <LazyLoadImage
+          effect="blur"
+          placeholderSrc={project.previewImage || '/images/placeholder.png'}
+          src={project.image || '/images/placeholder.png'}
+        />
       </div>
       <Card.Content>
         <Card.Header
